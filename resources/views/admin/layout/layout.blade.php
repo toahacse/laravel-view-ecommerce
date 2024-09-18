2089a26e54d5bf156febdf76fd759a2f2978daf7
toahacse <toahacse@gmail.com>
2024-09-18 09:23:51 +0600

    sidebar update

--- resources/views/admin/layout/layout.blade.php
+++ resources/views/admin/layout/layout.blade.php
@@ -336,7 +336,7 @@
 						<a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 							<img src="assets/images/avatars/avatar-2.png" class="user-img" alt="user avatar">
 							<div class="user-info ps-3">
-								<p class="user-name mb-0">Pauline Seitz</p>
+								<p class="user-name mb-0">{{Auth::user()->name ?? 'Admin'}}</p>
 								<p class="designattion mb-0">Web Designer</p>
 							</div>
 						</a>
@@ -345,11 +345,6 @@
 							</li>
 							<li><a class="dropdown-item" href="javascript:;"><i class="bx bx-cog"></i><span>Settings</span></a>
 							</li>
-							<li><a class="dropdown-item" href="javascript:;"><i class='bx bx-home-circle'></i><span>Dashboard</span></a>
-							</li>
-							<li><a class="dropdown-item" href="javascript:;"><i class='bx bx-dollar-circle'></i><span>Earnings</span></a>
-							</li>
-							<li><a class="dropdown-item" href="javascript:;"><i class='bx bx-download'></i><span>Downloads</span></a>
 							</li>
 							<li>
 								<div class="dropdown-divider mb-0"></div>
