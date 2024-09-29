798f78ec64d87d3e4d8496b3d1ac903f0e064be2
toahacse <toahacse@gmail.com>
2024-09-29 09:36:00 +0600

    update

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,12 +3,41 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+    use ApiResponse;
+
+    public function register(Request $request)
+    {
+        $validation = Validator::make($request->all(),[
+            'name'    => 'required|string|max:255',
+            'email'   => 'required|string|email|unique:users,email',
+            'password'=> 'required|string|min:6'
+        ]);
+   
+        if($validation->fails()){
+            return $this->error($validation->errors()->first(),400,[]);
+   
+        }
+ 
+        $user = User::create([
+            'name' => $request->name,
+            'password' => bcrypt($request->password),
+            'email' => $request->email
+        ]);
+ 
+        return $this->success([
+            'token' => $user->createToken('API Token')->plainTextToken
+        ]);
+    }
+
+
     public function loginUser(Request $request){
         $validation = Validator::make($request->all(), [
              'email' => 'required|string|email|exists:users,email',
